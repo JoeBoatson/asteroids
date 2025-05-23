243d351998e53926eb6401254d5e1ad92daee4f5
Joe <mlgxjofish@gmail.com>
2025-05-23 18:53:52 +0100

    added fps clock

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     game_loop = 5
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while game_loop > 0:
+    while game_loop != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill(000000)
-    pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
