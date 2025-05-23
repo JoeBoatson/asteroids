60a23042e9d17a27dfd42f66470dae462bc3fa7d
Joe <mlgxjofish@gmail.com>
2025-05-23 18:34:31 +0100

    Added window with working close

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
-
+from constants import *
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print ("Starting Asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    game_loop = 5
 
-
-
-
+    while game_loop > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    screen.fill(000000)
+    pygame.display.flip()
 if __name__ == "__main__":
     main()
